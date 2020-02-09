ef7fa0952f9a084b4368650019b06a35aeae8a94
Isabella Cho <isabellacho@1x-vl960-128-119-252-97.wireless.umass.edu>
2020-02-09 05:12:03 -0500

    button works!

--- button.js
+++ button.js
@@ -1,33 +1,10 @@
 function myFunction(name){
-
-    var user = "izzi";
-    var tmstmp = Date.now();
-    /*var newDBRef = firebase.database().ref("/test").push();
-    newDBRef.update({
-        reaction: name,
-        timestamp: tmstmp
-      }); */
-      var params = JSON.stringify({ "reaction": name});
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          //alert("no!");
-        }
-      };
-      xhttp.open('PUT', "https://reflec-46d2b.firebaseio.com/" + user + "/" + tmstmp + ".json", true);
-      xhttp.setRequestHeader('Content-type', 'application/json');
-      //console.log("hi");
-      console.log(params);
-
-      xhttp.send([params]);
-      
-      // curl -X PUT -d '{
-      //   "alanisawesome": {
-      //     "name": "Alan Turing",
-      //     "birthday": "June 23, 1912"
-      //   }
-      // }' 'https://reflec-46d2b.firebaseio.com/reacts.json'
-      
-
-
+  var user = "izzi";
+  var tmstmp = Date.now();
+  var params = JSON.stringify({ "reaction": name});
+  var xhttp = new XMLHttpRequest();
+    xhttp.open('PUT', "https://reflec-46d2b.firebaseio.com/" + user + "/" + tmstmp + ".json", true);
+    xhttp.setRequestHeader('Content-type', 'application/json');
+    console.log(params);
+    xhttp.send([params]);
 }
