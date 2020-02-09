a32b8565660a675fbaf99a417b5a77925e740453
Iman Anwarzai <imananwarzai@1x-vl927-72-19-107-5.wireless.umass.edu>
2020-02-09 08:51:49 -0500

    transformation fixed

--- transformation.py
+++ transformation.py
@@ -5,13 +5,31 @@ if __name__ == "__main__":
     url = 'https://reflec-46d2b.firebaseio.com/izzi.json'
     response = requests.get(url = url)
     react_data = response.json()
-    #print(react_data)
-    #react_seconds = list(react_data.keys()) 
-    #print(react_seconds[0])
+    # print(react_data)
     url = 'https://reflec-46d2b.firebaseio.com/speech.json'
     response = requests.get(url = url)
     speech_data = response.json()
-    #print (speech_data)
-   # for i in range(react_data[0]): iterate through these keys and search in the range of emotion keys
-    #    for i
-    #    we were thirsty \t \t happy happy sad
+    # print(speech_data)
+    react_seconds = list(react_data.keys()) 
+    speech_seconds = list(react_data.keys())
+    for i in range(len(speech_seconds)):
+        speech_values = list(speech_data.values())
+        if i >= len(speech_values):
+            break
+        linecase = list(speech_values[i].values())[1]
+        for j in range(len(react_seconds)):
+            react_values = list(react_data.values())
+            if int(react_seconds[j]) <= int(speech_seconds[i]):
+                linecase += " *"
+                linecase += list(react_values[j].values())[0]
+                linecase += "* "
+        print(linecase)
+
+    # apple = {'1581245767530': {'success': True, 'transcription': 'when you choose and honest beverage'}, '1581245779084': {'success': True, 'transcription': "organic and fair-trade certified p.m. ingredients you're making a small decision that creates"}}
+    # apple = speech_data
+    # something = list(apple.values())
+    # print(list(something[0].values())[1])
+
+# tup1 = ('physics', ('chemistry', 1997, 2000))
+# print(tup1[1])
+# print(tup1[1][1])
