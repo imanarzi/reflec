2a1ba0f3a7713340b0b43f7cedce813414cb773e
Isabella Cho <isabellacho@1x-vl960-128-119-252-97.wireless.umass.edu>
2020-02-09 05:02:50 -0500

    database live updating!!!!

--- button.js
+++ button.js
@@ -1,8 +1,33 @@
 function myFunction(name){
+
+    var user = "izzi";
     var tmstmp = Date.now();
-    firebase.database().ref("/reactions").set({
+    /*var newDBRef = firebase.database().ref("/test").push();
+    newDBRef.update({
         reaction: name,
         timestamp: tmstmp
-      });
-    console.log(name + " time: " + tmstmp);
+      }); */
+      var params = JSON.stringify({ "reaction": name});
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          //alert("no!");
+        }
+      };
+      xhttp.open('PUT', "https://reflec-46d2b.firebaseio.com/" + user + "/" + tmstmp + ".json", true);
+      xhttp.setRequestHeader('Content-type', 'application/json');
+      //console.log("hi");
+      console.log(params);
+
+      xhttp.send([params]);
+      
+      // curl -X PUT -d '{
+      //   "alanisawesome": {
+      //     "name": "Alan Turing",
+      //     "birthday": "June 23, 1912"
+      //   }
+      // }' 'https://reflec-46d2b.firebaseio.com/reacts.json'
+      
+
+
 }
