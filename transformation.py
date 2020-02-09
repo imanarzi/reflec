14a663cc5c831ad41cecaf4bac3ecc2dad923ffb
Iman Anwarzai <imananwarzai@1x-vl927-72-19-107-5.wireless.umass.edu>
2020-02-09 09:08:11 -0500

    trasnformation

--- transformation.py
+++ transformation.py
@@ -12,17 +12,19 @@ if __name__ == "__main__":
     # print(speech_data)
     react_seconds = list(react_data.keys()) 
     speech_seconds = list(react_data.keys())
+    j = 0
     for i in range(len(speech_seconds)):
         speech_values = list(speech_data.values())
         if i >= len(speech_values):
             break
         linecase = list(speech_values[i].values())[1]
-        for j in range(len(react_seconds)):
+        while j < len(react_seconds):
             react_values = list(react_data.values())
             if int(react_seconds[j]) <= int(speech_seconds[i]):
                 linecase += " *"
                 linecase += list(react_values[j].values())[0]
                 linecase += "* "
+            j += 1
         print(linecase)
 
     # apple = {'1581245767530': {'success': True, 'transcription': 'when you choose and honest beverage'}, '1581245779084': {'success': True, 'transcription': "organic and fair-trade certified p.m. ingredients you're making a small decision that creates"}}
